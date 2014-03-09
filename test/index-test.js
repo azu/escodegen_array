aa6263b3dc16a6f3ecbda2f9fd4d1c4b8e73a4b9
azu <azuciao@gmail.com>
2014-03-10 00:33:21 +0900

    test(index): fix typo

--- test/index-test.js
+++ test/index-test.js
@@ -4,6 +4,6 @@ var assert = require("assert");
 // Array
 var esf = require("../");
 it("should return same input", function () {
-    var code = "fn([1,2,3])";
+    var code = "fn([1,2,3]);";
     assert.equal(esf(code), code);
 });
