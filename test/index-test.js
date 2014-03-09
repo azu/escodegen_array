41ecd13f6aa9dd3399a7bc2178820d47056dcb24
azu <azuciao@gmail.com>
2014-03-09 21:26:32 +0900

    refactor(test) use variable

--- test/index-test.js
+++ test/index-test.js
@@ -4,5 +4,6 @@ var assert = require("assert");
 // Array
 var esf = require("../");
 it("should return same input", function () {
-    assert.equal(esf("fn([1,2,3])"), "fn([1,2,3])");
+    var code = "fn([1,2,3])";
+    assert.equal(esf(code), code);
 });
